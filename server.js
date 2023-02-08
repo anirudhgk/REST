6ad4b3b3b10126fd6bf4afd01b1808150c80a7ae
Anirudh <anirudh@Anirudhs-MacBook-Pro.local>
2023-02-07 19:47:29 -0500

    REST file

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var mariadb = require('mariadb');
+var pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'sample',
+    port: 3306,
+    connectionLimit: 5
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello! Testing the api here!");
+});
+
+app.get('/customers', (req, res) => {
+    pool.getConnection()
+        .then(conn => {
+            conn.query("SELECT * FROM customer")
+                .then((rows) => {
+                    console.log(rows);
+                    res.setHeader('Content-Type', 'Application/json');
+                    res.send(rows);
+                })
+                .catch(err => {
+                    //handle error
+                    res.status(404);
+                    console.log(err);
+                    conn.end();
+                })
+        }).catch(err => {
+            //not connected
+            console.log("connection error with db", err);
+        });
+});
+
+// get agents
+app.get('/agents', function (req, res) {
+    pool.query('SELECT * FROM agents')
+        .then((rows) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'Application/json');
+            res.send(rows);
+        })
+        .catch(err => console.error('error with the db query', err.stack));
+});
